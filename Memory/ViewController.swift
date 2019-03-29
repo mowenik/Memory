43146bf8fed6f2956617b81ed4b970df9a3285c5
Максим Витовицкий <maxim@twincorp.ru>
2019-03-29 17:37:40 +0500

    Initial Commit

---
+++ Memory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memory
+//
+//  Created by Maxim Vitovitsky on 29/03/2019.
+//  Copyright © 2019 Максим Витовицкий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
